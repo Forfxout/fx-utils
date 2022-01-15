8682d4c75e7850cf3e32db7dae91037159e7cba5
desktop <main@forfxout.lan>
2022-01-15 22:52:54 +0300

    DaisyUI installed

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>
-    <div class="flex w-full justify-center">
-      <div class="flex flex-col">
-        <span class="text-2xl font-semibold">Welcome to FX-Utilities</span>
-        <span>Choose the app to work with</span>
-      </div>
+  <div class="p-10">
+    <div class="flex justify-center flex-col">
+      <section class="title">
+        <div class="flex flex-col text-center">
+          <span class="text-2xl font-semibold">Welcome to FX-Utilities</span>
+          <span class="text-lg">Choose the app to work with</span>
+        </div>
+      </section>
+
+      <section class="app-selector">
+        <div class="text-center">
+          <button class="btn btn-primary">Payoneer Calculator</button>
+        </div>
+      </section>
     </div>
   </div>
 </template>

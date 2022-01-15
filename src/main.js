0877c21d16b80c338030c4873e73f19e8fb485b2
desktop <main@forfxout.lan>
2022-01-15 22:29:46 +0300

    Tailwind changes

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 // Styling
-import './assets/css/index.css'
+import './assets/css/tailwind.css'
 
 import './registerServiceWorker'
 
 import router from './router'
 
 import store from './store'
-import './assets/tailwind.css'
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <div class="">
-      <span class="text-xl">Welcome to FX-Utilities</span>
-      <span>Choose the app to work with</span>
+    <div class="flex w-full justify-center">
+      <div class="flex flex-col">
+        <span class="text-2xl font-semibold">Welcome to FX-Utilities</span>
+        <span>Choose the app to work with</span>
+      </div>
     </div>
   </div>
 </template>

3ef808f27af19e9d43a4993288525daca7ee0e61
desktop <main@forfxout.lan>
2022-01-15 21:43:16 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Works!</div>
+  <router-view/>
+</template>
+
+<style lang="sass">
+#app
+  -moz-osx-font-smoothing: grayscale
+  -webkit-font-smoothing: antialiased
+  font-family: Avenir, Helvetica, Arial, sans-serif
+</style>
